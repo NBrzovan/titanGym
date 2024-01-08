00e1c71c0b65bce4e241ffa5bd8ff2cbdd4c68d3
Nikola <61702488+NBrzovan@users.noreply.github.com>
2024-01-08 13:39:55 +0100

    first commit

---
+++ resources/views/fixed/head.blade.php
@@ -0,0 +1,8 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="{{ asset('assets/vendors/mdi/css/materialdesignicons.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/vendors/css/vendor.bundle.base.css') }}">
+<title>@yield('title', 'Laravel App')</title>
+<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+<link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" />
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('fixed.head')
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
